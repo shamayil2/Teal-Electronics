ed85062c986518020d3fc0e2925cd6f61bef8c6a
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-02-21 00:40:26 +0530

    Made some visual changes to buttons on cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -43,7 +43,7 @@ const Cart = ({idsInCartObj,setIdsInCartObj,setWishlist,wishlist}) => {
         <h3>{product.title}</h3>
         <p>Quantity:<span><button onClick={(id)=>increaseQuantityInCart(product._id)}>+</button><input type="text" className="text-center" style={{width:"30px"}} value={idsInCartObj[product._id]}/><button onClick={(id)=>decreaseQuantityInCart(product._id)}>-</button></span></p>
         <button style={{padding:"0px 30px",backgroundColor:"#FF7F7F",color:"white",border:"1px solid #FF7F7F"}} onClick={(id)=>removeIdFromCart(product._id)}>Remove from Cart</button>
-        <button onClick={(id)=>moveToWishlistFromCart(product._id)} style={{padding:"0px 35px",backgroundColor:"#008080",color:"#F4F2DE"}}>Move to Wishlist</button>
+       {product._id in wishlist? <button style={{padding:"0px 35px",backgroundColor:"#F4F2DE",color:"#008080"}}>Go To Wishlist</button>:<button onClick={(id)=>moveToWishlistFromCart(product._id)} style={{padding:"0px 35px",backgroundColor:"#008080",color:"#F4F2DE"}}>Move to Wishlist</button>} 
         </div>
         
         </>)):loading && <p>Loading..</p>}
