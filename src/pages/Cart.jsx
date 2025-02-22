53269caa5d0f64e8663bf46de82efb34c391902f
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-02-23 00:31:21 +0530

    Finished Price Card in Cart Page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -34,7 +34,7 @@ const Cart = ({idsInCartObj,setIdsInCartObj,setWishlist,wishlist}) => {
     return(
         <>
         <Header/>
-        <main className="container">
+        <main className="container py-4">
         <h1 className="text-center">My Cart</h1>
         <div className="row">
             <div className="col-md-9">
@@ -43,7 +43,7 @@ const Cart = ({idsInCartObj,setIdsInCartObj,setWishlist,wishlist}) => {
         <div className="col-md-4">
          <img style={{height:"200px"}} className="img-fluid" src={product.productImage} alt="" />   
         <h3>{product.title}</h3>
-        <p>{product.price}</p>
+        <p>{product.originalPrice}</p>
         <p>Quantity:<span><button onClick={(id)=>increaseQuantityInCart(product._id)}>+</button><input type="text" className="text-center" style={{width:"30px"}} value={idsInCartObj[product._id]}/><button onClick={(id)=>decreaseQuantityInCart(product._id)}>-</button></span></p>
         <button style={{padding:"0px 30px",backgroundColor:"#FF7F7F",color:"white",border:"1px solid #FF7F7F"}} onClick={(id)=>removeIdFromCart(product._id)}>Remove from Cart</button>
        {product._id in wishlist? <button style={{padding:"0px 35px",backgroundColor:"#F4F2DE",color:"#008080"}}>Go To Wishlist</button>:<button onClick={(id)=>moveToWishlistFromCart(product._id)} style={{padding:"0px 35px",backgroundColor:"#008080",color:"#F4F2DE"}}>Move to Wishlist</button>} 
@@ -52,12 +52,17 @@ const Cart = ({idsInCartObj,setIdsInCartObj,setWishlist,wishlist}) => {
         </>)):loading && <p>Loading..</p>}
         </div>
         </div>
-        <div className="col-md-3 bg-light p-4">
+        <div style={{height:"50vh"}} className="col-md-3 bg-light p-4">
             <h1>Price Details</h1>
             <hr/>
-            <h5>Price ({Object.keys(idsInCartObj).length}) : {data?data.filter((product)=>product._id in idsInCartObj ).reduce((acc,curr)=>curr.price + acc,0):loading && <p>Loading...</p>}
+            <h5 className="fw-light">Price ({Object.keys(idsInCartObj).length}) : Rs.{data?data.filter((product)=>product._id in idsInCartObj ).reduce((acc,curr)=>(curr.originalPrice * idsInCartObj[curr._id]) + acc  ,0):loading && <p>Loading...</p>}
             </h5>
-            
+            <h5 className="fw-light">Total Discount : -Rs.{data? data.filter((product)=>product._id in idsInCartObj ).reduce((acc,curr)=>(curr.originalPrice * idsInCartObj[curr._id]) + acc,0) - data.filter((product)=>product._id in idsInCartObj).reduce((acc,curr)=>acc + (curr.price * idsInCartObj[curr._id]),0):loading && <p>Loading..</p>}</h5>
+            <h5 className="fw-light">Delivery Charges: Rs. 500</h5>
+            <hr/>
+            <h5 className="fw-normal">Total Price: Rs.{data && data.filter((product)=>product._id in idsInCartObj ).reduce((acc,curr)=>(curr.originalPrice * idsInCartObj[curr._id]) + acc,0) + 500}</h5>
+            <hr/>
+            <button style={{padding:"5px 80px"}} className="btn btn-primary">Place Order</button>
         </div>
         </div>
        
