b6ae8e83d060a4e06979fb315610b83b7b4e51b7
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-02-22 23:58:59 +0530

    Added Total Price card on Cart Page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -37,10 +37,13 @@ const Cart = ({idsInCartObj,setIdsInCartObj,setWishlist,wishlist}) => {
         <main className="container">
         <h1 className="text-center">My Cart</h1>
         <div className="row">
+            <div className="col-md-9">
+                <div className="row">
         {data?data.filter((product)=>product._id in idsInCartObj).map(product=> (<>
-        <div className="col-md-3">
+        <div className="col-md-4">
          <img style={{height:"200px"}} className="img-fluid" src={product.productImage} alt="" />   
         <h3>{product.title}</h3>
+        <p>{product.price}</p>
         <p>Quantity:<span><button onClick={(id)=>increaseQuantityInCart(product._id)}>+</button><input type="text" className="text-center" style={{width:"30px"}} value={idsInCartObj[product._id]}/><button onClick={(id)=>decreaseQuantityInCart(product._id)}>-</button></span></p>
         <button style={{padding:"0px 30px",backgroundColor:"#FF7F7F",color:"white",border:"1px solid #FF7F7F"}} onClick={(id)=>removeIdFromCart(product._id)}>Remove from Cart</button>
        {product._id in wishlist? <button style={{padding:"0px 35px",backgroundColor:"#F4F2DE",color:"#008080"}}>Go To Wishlist</button>:<button onClick={(id)=>moveToWishlistFromCart(product._id)} style={{padding:"0px 35px",backgroundColor:"#008080",color:"#F4F2DE"}}>Move to Wishlist</button>} 
@@ -48,6 +51,16 @@ const Cart = ({idsInCartObj,setIdsInCartObj,setWishlist,wishlist}) => {
         
         </>)):loading && <p>Loading..</p>}
         </div>
+        </div>
+        <div className="col-md-3 bg-light p-4">
+            <h1>Price Details</h1>
+            <hr/>
+            <h5>Price ({Object.keys(idsInCartObj).length}) : {data?data.filter((product)=>product._id in idsInCartObj ).reduce((acc,curr)=>curr.price + acc,0):loading && <p>Loading...</p>}
+            </h5>
+            
+        </div>
+        </div>
+       
         </main>
         </>
 
